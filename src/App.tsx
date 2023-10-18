ae2dd7611b18ff7d5d0117dde3f39cbd8ebc3691
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-18 17:01:45 -0230

    cleanup, tailwind installed, built ran

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -12,12 +11,9 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card text-3xl">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
