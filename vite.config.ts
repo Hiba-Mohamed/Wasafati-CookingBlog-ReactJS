825a7fe6a8f9ca08c9dc8a26dd89fd370ee3e663
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-19 17:10:24 -0230

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-    outDir: '/dist/Wasafati-CookingBlog-ReactJS', // Set the correct output directory
-  },
 })
