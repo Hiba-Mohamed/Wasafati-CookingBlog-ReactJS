3c6e62a7642d21465429b73c16410be3460b1f72
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-19 17:32:11 -0230

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@
 import { Routes, Route } from "react-router-dom";
 
 import { IntroPage } from "./pages/IntroPage";
+import { HashRouter } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<IntroPage />} />
+        <HashRouter>
+          <Route path="/" element={<IntroPage />} />
+        </HashRouter>
       </Routes>
     </div>
   );
