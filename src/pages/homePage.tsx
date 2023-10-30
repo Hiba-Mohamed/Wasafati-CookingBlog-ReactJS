f48064a9ce993f90136de436b933744b642d7e6f
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-10-30 11:19:28 -0230

    styling, title

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export function Homepage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center min-h-screen py-10">
       <div className="flex flex-col sm:flex-row items-center sm:flex-row-reverse  sm:p-2 max-w-6xl">
         <img
-          src="src/img/wasafati-banner.png"
+          src="/src/img/wasafati-banner.png"
           alt="Wasafati logo in the middle, and around it six pciture of various food and cooking ingredients"
           className="p-4 w-64 sm:w-96"
         />
